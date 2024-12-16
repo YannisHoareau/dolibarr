8d51b66e4ac29430a4617ec30ce54182522bdafe
yannis <yannishoareau26@gmail.com>
2024-12-16 16:07:29 +0100

    Changes to follow mvc logic

--- htdocs/subtotals/class/commonsubtotal.class.php
+++ htdocs/subtotals/class/commonsubtotal.class.php
@@ -233,30 +233,15 @@ trait CommonSubtotal
 	 */
 	public function getSubtotalForm($form, $langs, $type)
 	{
-		$langs->load('subtotals');
+		$tpl = dol_buildpath('/core/tpl/subtotal_create.tpl.php');
 
-		$depth_array = array();
-		for ($i = 0; $i < getDolGlobalString('SUBTOTAL_'.strtoupper($this->element).'_MAX_DEPTH', 2); $i++) {
-			$depth_array[$i + 1] = $langs->trans("Level", $i + 1);
+		if (empty($conf->file->strict_mode)) {
+			$res = @include $tpl;
+		} else {
+			$res = include $tpl; // for debug
 		}
 
-		// Create an array for form
-		$formquestion = array(
-			array('type' => 'hidden', 'name' => 'subtotallinetype', 'value' => $type),
-			array('type' => 'text', 'name' => 'subtotallinedesc', 'label' => $langs->trans("SubtotalLineDesc"), 'moreattr' => 'placeholder="'.$langs->trans("Description").'"'),
-			array('type' => 'select', 'name' => 'subtotallinelevel', 'label' => $langs->trans("SubtotalLineLevel"), 'values' => $depth_array, 'default' => 1, 'select_show_empty' => 0),
-		);
-
-		$page = $_SERVER["PHP_SELF"];
-		if ($this->element == 'facture') {
-			$page .= '?facid='.$this->id;
-		} elseif (in_array($this->element, array('propal', 'commande'))) {
-			$page .= '?id='.$this->id;
-		}
-
-		$form_title = $type == 'title' ? $langs->trans('AddTitleLine') : $langs->trans('AddSubtotalLine');
-
-		return $form->formconfirm($page, $form_title, '', 'confirm_add_line', $formquestion, 'yes', 1);
+		return $res;
 	}
 
 	/**
